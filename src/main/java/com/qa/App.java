a52651d137be8a82c9154d1f54ffcfae477fb455
Stu <Stu.scarth@gmail.com>
2026-03-10 16:23:01 +0000

    EDIT: Hello Everyone version 2.0-SNAPSHOT - added victoria

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -15,6 +15,7 @@ public class App
         System.out.println(sayHelloToSomeone("Ian"));
         System.out.println(sayHelloToSomeone("Nicola"));
         System.out.println(sayHelloToSomeone("Stu"));
+        System.out.println(sayHelloToSomeone("Victoria"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
